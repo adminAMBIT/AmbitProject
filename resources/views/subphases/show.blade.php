c6eec7e7337d8b2d2863845faac92d35fe80e968
Riba00 <jordi.riba.ferre@gmail.com>
2023-12-12 10:04:07 +0100

    remove second button and update vite

--- resources/views/subphases/show.blade.php
+++ resources/views/subphases/show.blade.php
@@ -241,16 +241,13 @@
                     <div class="sm:flex-auto">
                         <h1 class="text-xl font-medium text-gray-500">Documents</h1>
                     </div>
-                    <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                        <a type="button"
-                            href="{{ route('projects.phases.subphases.document.create', ['project_id'=>$subphase->phase->project->id, 'phase_id'=>$subphase->phase, 'subphase_id'=>$subphase->id]) }}"
-                            class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Upload
-                            Documents</a>
-                        <a type="button"
-                            href="{{ route('projects.phases.subphases.document.create', ['project_id'=>$subphase->phase->project->id, 'phase_id'=>$subphase->phase, 'subphase_id'=>$subphase->id]) }}"
-                            class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Upload
-                            Documents</a>
+                    <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none flex gap-4">
+                        <a href="{{ route('projects.phases.subphases.document.create', ['project_id'=>$subphase->phase->project->id, 'phase_id'=>$subphase->phase, 'subphase_id'=>$subphase->id]) }}"
+                           class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                            Upload Documents
+                        </a>
                     </div>
+                    
                 </div>
                 <div class="mt-8 flow-root">
                     <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
