10c2c75b5d8f43b334bbcb3169b3b86c4b1287d0
Riba00 <jordi.riba.ferre@gmail.com>
2023-12-14 09:07:17 +0100

    dashboard documents table

---
+++ app/Livewire/LastDocumentsTable.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Document;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use PowerComponents\LivewirePowerGrid\Button;
+use PowerComponents\LivewirePowerGrid\Column;
+use PowerComponents\LivewirePowerGrid\Exportable;
+use PowerComponents\LivewirePowerGrid\Facades\Filter;
+use PowerComponents\LivewirePowerGrid\Footer;
+use PowerComponents\LivewirePowerGrid\Header;
+use PowerComponents\LivewirePowerGrid\PowerGrid;
+use PowerComponents\LivewirePowerGrid\PowerGridColumns;
+use PowerComponents\LivewirePowerGrid\PowerGridComponent;
+use PowerComponents\LivewirePowerGrid\Traits\WithExport;
+
+final class LastDocumentsTable extends PowerGridComponent
+{
+    use WithExport;
+
+    public string $sortField = 'updated_at';
+    public string $sortDirection = 'desc';
+
+    public function setUp(): array
+    {
+        // $this->showCheckBox();
+
+        return [
+            // Exportable::make('export')
+            //     ->striped()
+            //     ->type(Exportable::TYPE_XLS, Exportable::TYPE_CSV),
+            Header::make(),
+            Footer::make()
+                ->showPerPage()
+                ->showRecordCount(),
+        ];
+    }
+
+    public function datasource(): ?Builder
+    {
+        $subquery = DB::table('documents')
+            ->leftJoin('companies', 'documents.company_id', '=', 'companies.id')
+            ->leftJoin('projects', 'documents.project_id', '=', 'projects.id') // Añadir esta línea
+            ->select('documents.*', 'companies.name as company_name', 'projects.title as project_name') // Añadir project_name
+            ->orderBy('documents.updated_at', 'desc')
+            ->limit(30)
+            ->toSql();
+
+
+        return Document::fromSub($subquery, 'alias_subquery');
+    }
+
+    public function relationSearch(): array
+    {
+        return [];
+    }
+
+    public function addColumns(): PowerGridColumns
+    {
+        return PowerGrid::columns()
+            ->addColumn('name', fn(Document $model) => $model->name . '.' . $model->extension)
+            ->addColumn('project_name')
+            ->addColumn('subphase_id', function (Document $model) {
+                return $model->subphase ? $model->subphase->name : null;
+            })
+            ->addColumn('company_name', function (Document $model) {
+                return $model->company_name ? $model->company_name : 'ADMIN';
+            })
+            ->addColumn('updated_at_formatted', fn(Document $model) => Carbon::parse($model->updated_at)->format('d/m/Y H:i:s'));
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make('Name', 'name')
+                ->sortable()
+                ->searchable(),
+
+            Column::make('Subphase', 'subphase_id')
+                ->sortable(),
+            Column::make('Project', 'project_name')->sortable()->searchable(),
+            Column::make('Company', 'company_name')
+                ->sortable()
+                ->searchable(),
+            Column::make('Updated at', 'updated_at_formatted', 'updated_at')
+                ->sortable(),
+            Column::action('Action')
+        ];
+    }
+
+    public function filters(): array
+    {
+        return [
+            Filter::inputText('name')->operators(['contains']),
+            Filter::inputText('company_name')->operators(['contains']),
+            Filter::inputText('project_name')->operators(['contains'])
+        ];
+    }
+
+
+    #[\Livewire\Attributes\On('show')]
+    public function show($rowId)
+    {
+        return redirect()->route('document.show', $rowId);
+    }
+
+    public function actions(Document $row): array
+    {
+        return [
+            Button::add('show')
+                ->slot('Show')
+                ->id()
+                ->class('pg-btn-white dark:ring-pg-primary-600 dark:border-pg-primary-600 dark:hover:bg-pg-primary-700 dark:ring-offset-pg-primary-800 dark:text-pg-primary-300 dark:bg-pg-primary-700')
+                ->dispatch('show', ['rowId' => $row->id])
+        ];
+    }
+
+    /*
+    public function actionRules($row): array
+    {
+       return [
+            // Hide button edit for ID 1
+            Rule::button('edit')
+                ->when(fn($row) => $row->id === 1)
+                ->hide(),
+        ];
+    }
+    */
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,7 +13,7 @@
     </x-slot>
 
     <div class="py-4">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-fit mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden py-4 shadow-xl sm:rounded-lg">
                 <div class="px-4 sm:px-6 lg:px-8">
                     <div class="sm:flex sm:items-center">
@@ -22,65 +22,12 @@
                             <p class="mt-2 text-sm text-gray-700">List of 30 documents have been updated recently.</p>
                         </div>
                     </div>
+
+                    
                     <div class="mt-8 flow-root">
                         <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                             <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-                                <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
-                                    <table class="min-w-full divide-y divide-gray-300">
-                                        <thead class="bg-gray-50">
-                                            <tr>
-                                                <th scope="col"
-                                                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
-                                                    Name</th>
-                                                <th scope="col"
-                                                    class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                                    Project</th>
-                                                <th scope="col"
-                                                    class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                                    User</th>
-                                                <th scope="col"
-                                                    class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                                    Updated at</th>
-                                                <th scope="col"
-                                                    class="px-3 py-3.5 text-center text-sm font-semibold text-gray-900">
-                                                    Actions</th>
-
-                                            </tr>
-                                        </thead>
-                                        <tbody class="divide-y divide-gray-200 bg-white">
-                                            @if (count($documents) == 0)
-                                            <tr>
-                                                <td colspan="5" class="text-center py-4">No documents found</td>
-                                            </tr>
-                                            @else
-                                            @foreach ($documents as $document)
-                                            <tr>
-                                                <td
-                                                    class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-                                                    {{ $document->name }}.{{ $document->extension }}</td>
-                                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{
-                                                    $document->project->title }}</td>
-                                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                                    @if ($document->company_id != null)
-                                                    {{ $document->user->name }} ({{ $document->company->name }})
-                                                    @else
-                                                    {{ $document->user->name }}
-                                                    @endif
-                                                </td>
-                                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{
-                                                    $document->updated_at->diffForHumans() }}
-                                                </td>
-                                                <td
-                                                    class="relative whitespace-nowrap py-4 text-center text-sm font-medium">
-                                                    <a href="{{ route('document.show', ['document_id'=>$document->id]) }}"
-                                                        class="text-indigo-600 font-bold hover:text-indigo-900">Show</a>
-                                                </td>
-                                            </tr>
-                                            @endforeach
-                                            @endif
-                                        </tbody>
-                                    </table>
-                                </div>
+                                <livewire:last-documents-table />
                             </div>
                         </div>
                     </div>
