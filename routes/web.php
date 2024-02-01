37ebd4b43e77633d5ccf270b0801d288965bbe91
Riba00 <jordi.riba.ferre@gmail.com>
2024-02-01 12:29:37 +0100

    stop manteniance

--- routes/web.php
+++ routes/web.php
@@ -26,14 +26,10 @@ use Illuminate\Support\Facades\Route;
 */
 // Coloca esto al final de tu archivo web.php
 
-Route::get('/{path?}', function(){
-    return view('manteniance.show');
-})->where('path', '.*')->name('maintenance');
 
-
-// Route::get('/', function () {
-//     return redirect(route('login'));
-// });
+Route::get('/', function () {
+    return redirect(route('login'));
+});
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
