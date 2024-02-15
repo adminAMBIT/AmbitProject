31e3e55963513e3c696b8b63966efb005c2b9909
Riba00 <jordi.riba.ferre@gmail.com>
2024-02-15 09:17:45 +0100

    hide normal user change status button

--- resources/views/companies/showDocuments.blade.php
+++ resources/views/companies/showDocuments.blade.php
@@ -68,10 +68,11 @@
                     </div>
                     <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
                         @if($documents->isNotEmpty())
+                        @if(auth()->user()->is_admin)
                         <a type="button"
                             href="{{ route('projects.companies.documents.status', ['project_id'=>$project->id, 'company_id'=>$company->id])}}"
                             class="inline-block rounded-md bg-gray-600 px-3 mr-2 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">Change Status</a>
-
+                        @endif
                         <a type="button"
                             href="{{ route('projects.companies.documents.downloadForm', ['project_id'=>$project->id, 'company_id'=>$company->id]) }}"
                             class="inline-block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Download
